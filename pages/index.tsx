66bf1c6ae895534022de6c60b7bbdb3fde8b5d29
Chris <chris.walker.beng@gmail.com>
2022-06-14 13:30:31 +0100

    Testing branch frontend new after deletion (#2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  //testing github actions
+  //testing branch frontend new after deletion
   return (
     <div className={styles.container}>
       <Head>

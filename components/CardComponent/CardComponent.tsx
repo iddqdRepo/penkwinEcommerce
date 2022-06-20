9aed3d68e528bbc1a9df082f534e28cce8da7755
iddqdRepo <chris.walker.beng@gmail.com>
2022-06-20 14:19:11 +0100

    Added ref functionality to cardComponent

--- components/CardComponent/CardComponent.tsx
+++ components/CardComponent/CardComponent.tsx
@@ -1,20 +1,89 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useRef } from "react";
 import styles from "./cardCompoment.module.css";
-import Image from "next/image";
 
 function CardComponent() {
-  const cardOneRef = useRef(null);
-  const cardTwoRef = useRef(null);
-  const cardThreeRef = useRef(null);
+  const cardSectionRef = useRef(null);
+  const cardOneRef = useRef<HTMLDivElement>(null);
+  const cardTwoRef = useRef<HTMLDivElement>(null);
+  const cardThreeRef = useRef<HTMLDivElement>(null);
+  const leftButtonRef = useRef<HTMLDivElement>(null);
+  const rightButtonRef = useRef<HTMLDivElement>(null);
+  const track = [{ center: false }, { center: true }, { center: false }];
+
+  const resetStyle = (...elements) => {
+    console.log("elements = ", elements);
+    elements.forEach((el) => {
+      el.current.style.marginRight = "0px";
+      el.current.style.marginLeft = "0px";
+    });
+    console.log("styles reset");
+  };
 
   const leftButtonClick = () => {
-    console.log("Left button clicked");
-    console.log(cardOneRef.current.offsetWidth);
+    // cardOneRef.current.style.marginRight = "0px";
+    // cardOneRef.current.style.marginLeft = "0px";
+    // cardThreeRef.current.style.marginRight = "0px";
+    // cardThreeRef.current.style.marginLeft = "0px";
+    resetStyle(cardOneRef, cardThreeRef);
+    rightButtonRef.current.className = `${styles.rightButtonContainer}`;
+    leftButtonRef.current.className = `${styles.leftButtonContainer}`;
+    //^if it's the middle focused
+    if (track[1].center === true) {
+      console.log(cardSectionRef.current.offsetWidth);
+      cardOneRef.current.className = `${styles.cardLeft}  ${styles.cardFocused}`;
+      cardOneRef.current.style.marginLeft = `${cardSectionRef.current.offsetWidth}px`;
+      cardTwoRef.current.className = `${styles.cardMiddle}  ${styles.cardUnfocused}`;
+      cardThreeRef.current.className = `${styles.cardRight}  ${styles.remove}`;
+      leftButtonRef.current.className = `${styles.leftButtonContainer}  ${styles.remove}`;
+      track[1].center = false;
+      track[0].center = true;
+    } else {
+      cardOneRef.current.className = `${styles.cardLeft}  ${styles.cardUnfocused}`;
+      cardTwoRef.current.className = `${styles.cardMiddle}  ${styles.cardFocused}`;
+      cardThreeRef.current.className = `${styles.cardRight}  ${styles.cardUnfocused}`;
+      leftButtonRef.current.className = `${styles.leftButtonContainer}`;
+      cardOneRef.current.style.marginRight = "0px";
+      cardOneRef.current.style.marginLeft = "0px";
+      track[1].center = true;
+      track[2].center = false;
+    }
+    console.log(track);
   };
 
   const rightButtonClick = () => {
-    console.log("Right button clicked");
-    // console.log(cardThreeRef.current.className);
+    resetStyle(cardOneRef, cardThreeRef);
+
+    rightButtonRef.current.className = `${styles.rightButtonContainer}`;
+    leftButtonRef.current.className = `${styles.leftButtonContainer}`;
+
+    if (
+      cardOneRef.current != null &&
+      cardTwoRef.current != null &&
+      cardThreeRef.current != null
+    ) {
+      if (track[1].center) {
+        console.log("rightButtonClicked");
+        console.log(cardSectionRef.current.offsetWidth);
+        cardOneRef.current.className = `${styles.cardLeft} ${styles.remove}`;
+        cardTwoRef.current.className = `${styles.cardMiddle} ${styles.cardUnfocused}`;
+        cardThreeRef.current.style.marginRight = `${cardSectionRef.current.offsetWidth}px`;
+        cardThreeRef.current.className = `${styles.cardLeft} ${styles.cardFocused}`;
+        rightButtonRef.current.className = `${styles.rightButtonContainer} ${styles.remove}`;
+        track[1].center = false;
+        track[2].center = true;
+      } else {
+        cardOneRef.current.className = `${styles.cardLeft} ${styles.cardUnfocused}`;
+        cardTwoRef.current.className = `${styles.cardMiddle} ${styles.cardFocused}`;
+        cardThreeRef.current.className = `${styles.cardRight} ${styles.cardUnfocused}`;
+        cardOneRef.current.style.marginRight = "0px";
+        cardOneRef.current.style.marginLeft = "0px";
+        track[1].center = true;
+        track[2].center = false;
+        leftButtonRef.current.className = `${styles.leftButtonContainer}`;
+      }
+    }
+    console.log(track);
   };
 
   return (
@@ -31,20 +100,25 @@ function CardComponent() {
       </div>
       <div className={styles.cardContainer}>
         <div className={styles.sliderWindow}>
-          <div className={styles.leftButtonContainer} onClick={leftButtonClick}>
+          <div
+            className={styles.leftButtonContainer}
+            ref={leftButtonRef}
+            onClick={leftButtonClick}
+          >
             <div className={styles.leftButtonImage}></div>
           </div>
+          {/*//^----------------------------------------------------  CARD LEFT */}
           <div
-            className={`${styles.cardLeft} ${styles.cardFocused}`}
-            style={{ marginLeft: "300px" }}
+            className={`${styles.cardLeft}  ${styles.cardUnfocused}`}
+            // style={{ marginLeft: "300px" }}
             ref={cardOneRef}
           >
-            <Image
+            <img
               className={styles.cardImage}
               src="/smallSliderPlantar.png"
               alt=""
             />
-            <div className={styles.cardSection}>
+            <div className={styles.cardSection} ref={cardSectionRef}>
               <div className={styles.cardTitle}>Plantar Fasciitis</div>
               <div className={styles.cardDescription}>
                 Get back to doing what you love. Penkwin&apos;s products are
@@ -53,11 +127,12 @@ function CardComponent() {
               <button className={styles.cardButton}>Learn More</button>
             </div>
           </div>
+          {/*//^----------------------------------------------------  CARD MIDDLE */}
           <div
-            className={`${styles.cardMiddle} ${styles.cardUnfocused}`}
+            className={`${styles.cardMiddle} ${styles.cardFocused}`}
             ref={cardTwoRef}
           >
-            <Image
+            <img
               className={styles.cardImage}
               src="/smallSliderBunion.png"
               alt=""
@@ -71,12 +146,33 @@ function CardComponent() {
               <button className={styles.cardButton}>Learn More</button>
             </div>
           </div>
+          {/*//^----------------------------------------------------  CARD RIGHT */}
           <div
-            className={`${styles.cardRight} ${styles.cardUnocused} ${styles.remove} `}
+            className={`${styles.cardLeft}  ${styles.cardUnfocused}`}
+            ref={cardThreeRef}
+          >
+            <img
+              className={styles.cardImage}
+              src="/smallSliderPlantar.png"
+              alt=""
+            />
+            <div className={styles.cardSection} ref={cardSectionRef}>
+              <div className={styles.cardTitle}>Plantar Fasciitis</div>
+              <div className={styles.cardDescription}>
+                Get back to doing what you love. Penkwin&apos;s products are
+                there for you, every step of the way.
+              </div>
+              <button className={styles.cardButton}>Learn More</button>
+            </div>
+          </div>
+
+          {/* <div
+            className={`${styles.cardRight} ${styles.cardUnfocused}`}
+            // ${styles.remove}
             // style={{ marginRight: "300px" }}
             ref={cardThreeRef}
           >
-            <Image
+            <img
               className={styles.cardImage}
               src="/smallSliderTailbone.png"
               alt=""
@@ -89,10 +185,12 @@ function CardComponent() {
               </div>
               <button className={styles.cardButton}>Learn More</button>
             </div>
-          </div>
+          </div> */}
+
           <div
             className={styles.rightButtonContainer}
             onClick={rightButtonClick}
+            ref={rightButtonRef}
           >
             <div className={styles.rightButtonImage}></div>
           </div>

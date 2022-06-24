a35a5f9b2e5e309a400aeb9622e907c44ffc5569
iddqdRepo <chris.walker.beng@gmail.com>
2022-06-24 16:14:58 +0100

    Added more dummy data

---
+++ dummyData/categories.js
@@ -0,0 +1,14 @@
+const categories = {
+  id: "randomId",
+  categories: [
+    "All Products",
+    "Medical Cushions",
+    "Bunion Care",
+    "Arthritis Treatment",
+    "Plantar Fasciitis",
+    "Metatarsal Pain",
+    "Eye Care",
+  ],
+};
+
+export default categories;
--- dummyData/products.js
+++ dummyData/products.js
@@ -7,33 +7,54 @@ const products = [
       variantType: "Size",
       variants: [
         {
-          variantTitle: "15 inch",
-          sku: "",
-          price: "",
-          Quantity: "",
+          title: "15 inch",
+          sku: "15inch",
+          price: "11.99",
+          Quantity: "12",
         },
         {
-          variantTitle: "18 inch",
-          sku: "",
-          price: "",
-          Quantity: "",
+          title: "18 inch",
+          sku: "18inch",
+          price: "14.99",
+          Quantity: "0",
         },
       ],
     },
-    images: [],
+    images: "",
     shortDescription: {
       boldTitle: "",
-      description: "",
+      description:
+        "The Penkwin Orthopaedic Ring Pillow Dramatically Reduces Pain and Helps You to Rest Easy.\nImprove your quality of life with a medical pillow designed to reduce pressure and remove discomfort from the part of your body where you feel it most\n\nThe Inflatable Cushion is specially designed to provide gentle support everywhere, whether in the car, shower, sofa or wheelchair.",
       boldCarePackageText: "In each care package you will receive:",
-      packageContents: ["", ""],
+      packageContents: [
+        "1 x Inflatable Ring Cushion",
+        "1 x Travel friendly pump",
+      ],
     },
-    description: { title: "", bullets: [], image: [] },
+    description: {
+      title: "GET INSTANT RELIEF WITH THIS FULLY ADJUSTABLE PILLOW",
+      headerText:
+        "This spectacular cushion moulds to your body and your needs quickly, it$apos;s as simple as changing the air inside.",
+      bullets: [
+        "We only use medical grade materials so you can rest assured that your pillow won$apos;t crack, deflate or otherwise let you down when you need it most.",
+        "Completely hypoallergenic and machine washable you can avoid those embarrassing squeaks and smells and focus on getting you back to being you.",
+        "Travel Friendly: the Penkwin pillow folds down nice and small so you can take your pocket-sized pain relief anywhere",
+        "Soft Touch Material: Gentle on sensitive skin, the fabric won$apos;t stick to you or leave ugly marks",
+        "100% Satisfaction Guarantee: At Penkwin we believe in helping you get better. If for any reason you are not happy with your purchase, we will replace or refund, no questions asked.",
+      ],
+      image:
+        "https://cdn.shopify.com/s/files/1/1225/6296/files/Website_Main_png.png?v=1569948423",
+    },
     faq: [
       {
         "What does it do exactly":
           "The Penkwin® Cushion helps redistribute your body weight over the ischial tuberosities (aka 'sit bones') to prevent pressure on the perineum or pelvic floor.",
         "Who is it for ?":
           "It's perfect for anyone in need of a bit of extra support. It can be especially helpful for those experiencing, Pain from Prolonged Sitting, Bed Sores, Pressure Sores, Decubitus Ulcers, Chronic Pelvic Pain Syndrome (CPPS), Hemorrhoids, Hemorrhoid surgery, Pregnancy, Child Birth, Episiotomy, Rectal Fissures, Rectal Tearing, Rectal Surgery, Perineal Surgery, Prostatitis, Prostatectomy, Prostate Biopsy.",
+        "I am 250 lbs, am I too heavy?":
+          "Not at all! the cushion can comfortably help people up to 300lbs.",
+        "Can it be used in the bath or shower?":
+          "Absolutely! unlike foam pillows the Penkwin® Orthopaedic Ring Pillow can be used in the bath or shower. Just leave it to air dry afterwards so it doesn$apos;t stay damp.",
       },
     ],
 
@@ -44,6 +65,64 @@ const products = [
     reviews: [
       { name: "Darryl", date: "", rating: 5, review: "High quality" },
       { name: "John", date: "", rating: 1, review: "Low quality" },
+      { name: "Sharon", date: "", rating: 1, review: "Only comes in pairs" },
+    ],
+  },
+
+  {
+    id: 2,
+    title: "Penkwin® 6 Piece Metatarsal Relief Set",
+    variant: {
+      multiple: false,
+      variantType: "",
+      variants: [
+        {
+          variantTitle: "",
+          sku: "met01",
+          price: "11.99",
+          Quantity: "132",
+        },
+      ],
+    },
+    images: [
+      "https://cdn.shopify.com/s/files/1/1225/6296/products/71M7WJaGehL._SL1500_1024x1024@2x.jpg?v=1569942149",
+      "https://cdn.shopify.com/s/files/1/1225/6296/products/71frXHBNPIL._SL1500__1_1024x1024@2x.jpg?v=1569942149",
+      "https://cdn.shopify.com/s/files/1/1225/6296/products/71GqHLw9ysL._SL1500_1024x1024@2x.jpg?v=1569942149",
+      "https://cdn.shopify.com/s/files/1/1225/6296/products/71wZAj5Aw2L._SL1500_1024x1024@2x.jpg?v=1569942149",
+      "https://cdn.shopify.com/s/files/1/1225/6296/products/81J8Q7qqV_L._SL1500_1024x1024@2x.jpg?v=1569942149",
+    ],
+    shortDescription: {
+      boldTitle: "Feel like you're walking on a cloud",
+      description:
+        "This Complete 6-Piece Metatarsal Kit dramatically reduces pain in and out of shoes.\n This comprehensive kit is the perfect package to squash that pesky foot pain.",
+      boldCarePackageText: "In each care package you will receive:",
+      packageContents: [
+        "2 x metatarsal cushions",
+        "2 x adhesive gel cushions",
+        "2 x adhesive sandal gel cushions.",
+      ],
+    },
+    description: {
+      title: "SAY GOODBYE TO FOOT PAIN.",
+      bullets: [
+        "Are you tired of Morton&apos;s neuroma, metatarsal pain or aching in the balls of your feet? Our customers have reported instant pain relief where steroid injections have failed.",
+        "Super Effective for relieving pain caused by long-term walking, standing or wearing hard soles. Suitable for all shoe types, athletic shoes, high heels, or work boots",
+        "Protect your feet from blisters and abrasion even in flip flops! Beautifully soft and strong material can support you even in your summer footwear.",
+        "Soft Touch Material: This kit is made from super soft and gentle PU and SEBS material so you can be sure it won$apos;t stick to you or leave ugly marks.",
+      ],
+      image:
+        "https://cdn.shopify.com/s/files/1/1225/6296/files/small_png_beb83b52-2ad2-4158-800e-01c42f510db5.png?v=1569948253",
+    },
+    faq: [{}],
+
+    category: ["Metatarsal Pain", "all"],
+    sale: { onSale: false, salePrice: "" },
+    featured: false,
+    rating: 5,
+    reviews: [
+      { name: "Darryl", date: "", rating: 5, review: "High quality" },
+      { name: "John", date: "", rating: 1, review: "Low quality" },
+      { name: "Sharon", date: "", rating: 1, review: "Only comes in pairs" },
     ],
   },
 ];

dc136b94cfa27cf0b6d8502519810c7f45f657e2
iddqdRepo <chris.walker.beng@gmail.com>
2022-06-29 15:51:21 +0100

    Renamed pages to index

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./contact.module.css";
 
 function Contact() {
-  return <div className={styles.placeholder}>contact</div>;
+  return <div className={styles.placeholder}>contact Page</div>;
 }
 
 export default Contact;

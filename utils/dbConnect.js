b0aaa674d0fadff6002b44af39ab1d3435cbcd8c
iddqdRepo <chris.walker.beng@gmail.com>
2022-06-30 15:31:42 +0100

    Added mongoose and dbConnect

---
+++ utils/dbConnect.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+//check if we're connected to the database, if not then set up a connection.
+async function dbConnect() {
+  if (connection.isConnected) {
+    return;
+  }
+  const db = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  connection.isConnected = db.connections[0].readyState;
+  console.log(connection.isConnected);
+}
+
+export default dbConnect;

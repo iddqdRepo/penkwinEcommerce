4deb689191eab1a0eb220635912dcbdc77d749bc
iddqdRepo <chris.walker.beng@gmail.com>
2022-07-05 14:34:40 +0100

    Added additional data to categoryModel

--- Models/categoryModel.ts
+++ Models/categoryModel.ts
@@ -2,6 +2,13 @@ import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
   category: { type: String, required: true },
+  heroImage: { type: String, required: true },
+  title: { type: String, required: true },
+  subTitle: { type: String, required: true },
+  description: { type: String, required: true },
+  cardTitle: { type: String, required: true },
+  cardImage: { type: String, required: true },
+  cardBullets: { type: [], required: true },
 });
 
 export default mongoose.models.categories ||

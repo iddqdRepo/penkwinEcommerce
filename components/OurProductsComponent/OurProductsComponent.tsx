d8d41ce6ea59b67258c913d35de13c7c8ec0fac2
iddqdRepo <chris.walker.beng@gmail.com>
2022-07-12 11:40:39 +0100

    Made OurProductsComponents dynamically generate

--- components/OurProductsComponent/OurProductsComponent.tsx
+++ components/OurProductsComponent/OurProductsComponent.tsx
@@ -1,137 +1,71 @@
 /* eslint-disable @next/next/no-img-element */
+import { Key } from "react";
 import styles from "./ourProductsComponent.module.css";
 
-function OurProducts() {
+interface ProductsInterface {
+  Products: {
+    images: string;
+    title: string;
+    price: string;
+    [key: string]: any;
+  }[];
+}
+
+function OurProducts({
+  products,
+}: {
+  products: ProductsInterface["Products"];
+}) {
+  function MultipleVariants({
+    image,
+    title,
+    price,
+  }: {
+    image: string;
+    title: string;
+    price: string;
+  }) {
+    return (
+      <div className={styles.favouriteProductContainer}>
+        <div>
+          <img className={styles.image} src={image} alt="" />
+        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.price}>£{price}</div>
+        <button className={styles.learnMoreButton}>Learn More</button>
+      </div>
+    );
+  }
   return (
     <>
       <div className={styles.productsSectionContainer}>
         <div className={styles.productsContainer}>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Website_Main_png_720x.png?v=1569942122"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® Inflatable Orthopaedic Ring Cushion
-            </div>
-            <div className={styles.price}>£10.99</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/6_piece_Metatarsal_Main_png_720x.png?v=1569942111"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® Inflatable Orthopaedic Ring Cushion
-            </div>
-            <div className={styles.price}>£8.99</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
-          <div className={styles.favouriteProductContainer}>
-            <div>
-              <img
-                className={styles.image}
-                src="https://cdn.shopify.com/s/files/1/1225/6296/products/Main_png_720x.png?v=1569942128"
-                alt=""
-              />
-            </div>
-            <div className={styles.title}>
-              Penkwin® 5 piece Bunion Corrector Kit
-            </div>
-            <div className={styles.price}>£11.96</div>
-            <button className={styles.learnMoreButton}>Learn More</button>
-          </div>
+          {products ? (
+            products.map((item) => {
+              return item.variant.multiple ? (
+                <MultipleVariants
+                  key={(item.title + item.price) as Key}
+                  image={item.images[0]}
+                  title={item.title}
+                  price={item.variant.variants[0].price}
+                />
+              ) : (
+                <div
+                  key={(item.title + item.price) as Key}
+                  className={styles.favouriteProductContainer}
+                >
+                  <div>
+                    <img className={styles.image} src={item.images[0]} alt="" />
+                  </div>
+                  <div className={styles.title}>{item.title}</div>
+                  <div className={styles.price}>£{item.price}</div>
+                  <button className={styles.learnMoreButton}>Learn More</button>
+                </div>
+              );
+            })
+          ) : (
+            <div> No products available </div>
+          )}
         </div>
       </div>
     </>

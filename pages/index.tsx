fda408fec2bfe484281c4cc018605bad7ab81636
iddqdRepo <chris.walker.beng@gmail.com>
2022-07-13 11:40:01 +0100

    Made infoComponent dynamic

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,22 @@ const Home = () => {
       <CustomerFavouritesContainer />
       <div className={styles.spacerHundred}></div>
 
-      <InfoComponent />
+      <InfoComponent
+        title={"It's our risk, not yours."}
+        desc={
+          "Penkwin is so proud to have helped over 50,000 people all over the world. We love our customers and our customers love us! This is why we offer a lifetime guarantee with no questions asked returns and refunds on all our products."
+        }
+        image={"/50kProductsSold.png"}
+        imageSide={"right"}
+      />
+      <InfoComponent
+        title={"We work with you, not against you."}
+        desc={
+          "Penkwin is a company that specialise in various ranges of foot care products, orthotics and inflatable cushions. We are Northern Ireland based company that is dedicated to providing 100% medical grade, affordable products, 5* customer service and brilliant prices to all of our customers."
+        }
+        image={"/handHeartImage.png"}
+        imageSide={"left"}
+      />
       <div className={styles.spacerHundred}></div>
 
       <TitleHeaderComponent title={"Featured Products"} description={""} />

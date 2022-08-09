6b4a7a90777893113db9160ef3af65b63f7f5705
iddqdRepo <chris.walker.beng@gmail.com>
2022-08-09 11:29:03 +0100

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/login/index.{js,ts,jsx,tsx}", "./pages/login/index.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

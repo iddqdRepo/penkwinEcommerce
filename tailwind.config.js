cbe897fb6c21667f28c4abb7a5489d045801ea8c
iddqdRepo <chris.walker.beng@gmail.com>
2022-08-15 14:27:12 +0100

    Added admin sidebar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/login/index.{js,ts,jsx,tsx}",
     "./pages/login/index.tsx",
     "./pages/instructions/index.tsx",
+    "./pages/dashboard/*.tsx",
     "./components/NavbarComponent/NavbarComponent.tsx",
+    "./adminComponents/**/*.tsx",
   ],
   theme: {
     extend: {
